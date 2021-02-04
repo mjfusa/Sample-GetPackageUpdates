a076ac94f4747fe6f5faf6c41ab219ea0dd73f0e
Mike Francis <mike.francis@live.com>
2021-02-04 12:24:53 -0800

    initial commit

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+using Windows.Foundation;
+using Windows.Services.Store;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+
+    }
+
+
+
+
+}
